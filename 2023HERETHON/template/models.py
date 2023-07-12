bf44c0cb39bc3469b0e28c515a0880cc0b5706de
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-07-13 01:58:46 +0900

    HERETON/urls.py수정

--- 2023HERETHON/template/models.py
+++ 2023HERETHON/template/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Template(models.Model):
+    class Cate(models.IntegerChoices):
+        SCHOOL = 1
+        CLUB = 2
+        BASE = 3
+        MY = 4
+
+    id = models.AutoField(primary_key=True)
+    cate = models.IntegerField(choices=Cate.choices)
+    title = models.TextField()
+    greet = models.TextField()
+    intro = models.TextField()
+    content = models.TextField()
+    bye = models.TextField()
+
+    def __str__(self):
+        return "{}:{}-{}".format(self.cate,self.id,self.title)
