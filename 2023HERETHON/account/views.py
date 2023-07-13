9aa11162bf52631ec1fb39a3bb771c7fb9d18243
yoyoung02 <yoyoung0202@gmail.com>
2023-07-13 17:57:18 +0900

    로그인/회원가입페이지 연결(중복확인 제외)

--- 2023HERETHON/account/views.py
+++ 2023HERETHON/account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
-from django.contrib import messages
+#from django.contrib import messages
 
 def login(request):
     cate=1
@@ -25,15 +25,14 @@ def signup(request):
         password2 = request.POST['password2']
         
         if User.objects.filter(username=username).exists():
-            messages.error(request, '이미 존재하는 아이디입니다.')
-            return render(request, 'signup.html')
+            #messages.error(request, '이미 존재하는 아이디입니다.')
+            return render(request, 'signup.html', {'error': '이미 존재하는 아이디입니다.'})
 
         if request.POST['password'] == request.POST['password2']:
             user = User.objects.create_user(username=request.POST['userid'], password=request.POST['password'])
             auth.login(request, user)
             return render(request, 'login.html')
         else:
-            messages.error(request, '비밀번호를 동일하게 입력하세요.')
             return render(request, 'signup.html')
         
     return render(request, 'signup.html')
