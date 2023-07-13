8952085971e8e7681fcbb88e30e5288811f132fe
yoyoung02 <yoyoung0202@gmail.com>
2023-07-13 10:56:02 +0900

    HERETHON/urls.py에서 path 추가

--- 2023HERETHON/HERETHON/urls.py
+++ 2023HERETHON/HERETHON/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path('account/',include('account.urls')),
-    path('template/',include('template.urls'))
+    path('account/',include('account.urls')),
+    path('template/',include('template.urls')),
 ]
