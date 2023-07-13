a8619479abb70689ad6715d0f6c3297fa56b0805
yoyoung02 <yoyoung0202@gmail.com>
2023-07-13 14:46:32 +0900

    아이디중복확인/회원가입시 비밀번호 일치 기능 구현

--- 2023HERETHON/account/views.py
+++ 2023HERETHON/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     if request.method == "POST":
@@ -23,13 +24,15 @@ def signup(request):
         password2 = request.POST['password2']
         
         if User.objects.filter(username=username).exists():
-            return render(request, 'signup.html', {'error': '이미 존재하는 아이디입니다.'})
-        
+            messages.error(request, '이미 존재하는 아이디입니다.')
+            return render(request, 'signup.html')
+
         if request.POST['password'] == request.POST['password2']:
             user = User.objects.create_user(username=request.POST['userid'], password=request.POST['password'])
             auth.login(request, user)
             return render(request, 'login.html')
         else:
-            return render(request, 'register.html', {'error': '비밀번호를 동일하게 입력하세요.'})
+            messages.error(request, '비밀번호를 동일하게 입력하세요.')
+            return render(request, 'signup.html')
         
     return render(request, 'signup.html')
