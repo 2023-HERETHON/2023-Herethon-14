866db53cc69cd8655bffc8c580dd1818f2baf82a
yoyoung02 <yoyoung0202@gmail.com>
2023-07-15 03:41:17 +0900

    db 수정 후 push

--- 2023HERETHON/contact/models.py
+++ 2023HERETHON/contact/models.py
@@ -6,8 +6,8 @@ from account.models import User
 class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     con_user = models.ForeignKey(User,related_name='con_user',on_delete=models.CASCADE)
-    subject = models.CharField(max_length=30)
-    professor_name = models.CharField(max_length=10)
+    subject = models.CharField(max_length=30, null=True)
+    professor_name = models.CharField(max_length=10, null=True)
     email = models.EmailField(max_length=100)
     
     def __str__(self):
